13152e63f2fb56f257724c0985e79b0663f2759a
Adeyemi Olalekan <deyemibowen@gmail.com>
2025-12-04 11:37:01 +0100

    first commit

---
+++ PYTHON SHIT/Python_intro.py
@@ -0,0 +1,56 @@
+
+
+
+#collect an input from the user on terminal and call it player1_input
+
+#collect another input from the user an call it player2_input
+
+#check if the input entered by the player is rock or paper or sciccors
+
+#check if both input are the same and display TIE if they are 
+
+#check if the first input is sciccors and the second input is paper then display player1_input wins 
+ #otherwise if vise vasal display player2_input
+
+#check if the first input is sciccors and the second input is rock then display player2_input wins
+ #other wise if in reverse display player_1_input wins
+
+#check if the first input is rock and the second input is paper then display player2_input wins
+#otherwise display player1_input wins
+
+
+#function is any keyword that requires a parenthesis beside it 
+
+age = 50
+
+name = 'Delight is sleeping'
+
+print ("Marvelous", 42,12,67.90,83, "You LIe",age,name)
+
+message = "Enter a fvcking number: "
+
+player1_input = input(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
